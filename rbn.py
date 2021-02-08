de364ab4f05fbbeaa881a91083abb293edc660af
Jeff Schulz <jeffschulz7@gmail.com>
2021-02-08 22:09:55 +0000

    with more epochs and bigger alpha, we get somewhere. Still curious on why classification seems to happen in large circles - expecting behavior closer to KNN

--- rbn.py
+++ rbn.py
@@ -28,8 +28,9 @@ class RBN:
     def rbf(self, x, mu, sigma):
         # return np.exp(-1 * .5 * (1/np.power(sigma, 2)) * np.linalg.norm(x-mu))
         cov = [[sigma[0], 0], [0, sigma[1]]]
-        output = ( np.exp( -.5 * np.dot(np.dot(( x - mu ).T, np.linalg.inv(cov)) , ( x - mu )) ) / 
-                    ( np.power( np.power(2*np.pi, len(x)) * np.linalg.det(cov) , .5) ) )
+        # output = ( np.exp( -.5 * np.dot(np.dot(( x - mu ).T, np.linalg.inv(cov)) , ( x - mu )) ) / 
+        #             ( np.power( np.power(2*np.pi, len(x)) * np.linalg.det(cov) , .5) ) )
+        output = ( np.exp( -.5 * np.dot(np.dot(( x - mu ).T, np.linalg.inv(cov)) , ( x - mu )) ) )
         
         return output
 
@@ -74,7 +75,6 @@ class RBN:
 
 
 
-
     ##############################################
     # Backward Pass
     ##############################################
@@ -87,6 +87,7 @@ class RBN:
 
         return error
         
+
     def plot_stuff(self, *args):
         fig, ax = plt.subplots()
         for arr in args:
@@ -97,7 +98,7 @@ class RBN:
     ##############################################
     # Train: k-means then forward/backward
     ##############################################
-    def train(self, data, labels, alpha=.01, epochs=100, batch_size=50, error_target=.01):
+    def train(self, data, labels, alpha=.01, epochs=100, batch_size=50, dw_target=.01):
         # hidden layer output
         self.iota = np.zeros((self.k, self.o))
 
@@ -119,6 +120,7 @@ class RBN:
         self.plot_stuff(data, self.centers)
 
         ## Epochs
+        last_20 = []
         epochs_error = []
         for k in tqdm(range(epochs)):
             labels_ep = []
@@ -142,10 +144,14 @@ class RBN:
 
             batch_avg_error = np.sum(batch_error) / batch_size
 
-            # faux momentum?
-            if(abs(batch_avg_error) < alpha*.5):
-                alpha = alpha / 3.0
-                print('new alpha:', alpha)
+            # # faux momentum?
+            # last_20.append(abs(batch_avg_error))
+            # while(len(last_20) > 20):
+            #     last_20.pop(0)
+            # if(len(last_20) == 20 and abs(np.average(last_20) - last_20[-1]) < .0001):
+            #     alpha = alpha / 3.0
+            #     print('new alpha:', alpha)
+            #     last_20 = []
 
             if(abs(batch_avg_error) < best_error):
                 best_error   = abs(batch_avg_error)
@@ -153,18 +159,20 @@ class RBN:
 
             epochs_error.append(batch_avg_error)
             # Break if below error target
-            if(np.linalg.norm(self.hl_weights - old_h1) < error_target):
+            if(np.linalg.norm(self.hl_weights - old_h1) < dw_target):
                 break
 
         self.hl_weights = best_weights
 
-        # dist of outputs/labels
-        # fixed bin size
-        print('Hist of iota')
-        bins = np.arange(-3, 3, .01) # fixed bin size
-        plt.xlim([min(self.iota)-1, max(self.iota)+1])
-        plt.hist(self.iota, bins=bins, alpha=0.5)
-        plt.show()
+        # # dist of outputs/labels
+        # # fixed bin size
+        # print('Hist of iota')
+        # new_iota = [i for i in self.iota if i > 1e-5] # take out zeros
+        # print(len(new_iota))
+        # bins = np.arange(-3, 3, .01) # fixed bin size
+        # plt.xlim([min(new_iota)-1, max(new_iota)+1])
+        # plt.hist(new_iota, bins=bins, alpha=0.5)
+        # plt.show()
 
         # print('Hist of outputs')
         # bins = np.arange(-5, 5, .1) # fixed bin size
@@ -198,170 +206,47 @@ class RBN:
         return soft_labels
 
 
+    # #############################
+    # # Show confusion matrix
+    # def show_cm(self, show=True):
+    #     if(self.did_i_test):
+    #         cm = np.zeros((10,10))
 
-    ###############################################
-    # Testing
-    def test(self, test_dim=0, rand=True, ):
-        #Find test_dim
-        if(test_dim == 0):
-            test_dim = self.test_dim
-        elif(test_dim > self.test_dim):
-            print("\n\nWarning! Number of testing images ("+str(test_dim)+") too large. Increase test_dim parameter (currently "+str(self.test_dim)+") on object initialization.")
-            test_dim = self.test_dim
-        print("\nTesting on",test_dim,"images...") 
-        self.test_dim = test_dim
+    #         good = 0
+    #         for i in range(self.test_dim):
+    #             ind = int(self.pred_ind[i])
+    #             tpi = np.argmax(self.test_labels[ind])
+    #             cm[tpi] += self.pred_res[i]
 
-        self.pred_res  = np.zeros((test_dim,10))   
-        self.pred_ind  = np.zeros((test_dim,))
+    #             #score
+    #             if(tpi == np.argmax(self.pred_res[i])):
+    #                 good += 1
 
-        v1  = np.ones((1,101))
-        o   = np.ones((1,10))
-        ind = []
+    #         mx = np.max(cm)
 
-        ## Input Data
-        for l in progressbar(range(test_dim)):
-            # Can decide with 'rand' parameter to test in order
-            if(rand):
-                # Get random index
-                i = np.random.randint(low=0, high=self.test_dim, )
-                while(i in ind):
-                    i = np.random.randint(low=0, high=self.test_dim, )
-                ind.append(i)
-            else:
-                i = l
+    #         fig = plt.figure(figsize=(9,9))
+    #         fig.suptitle('')
 
-            x = self.test_data[i]
+    #         ax = fig.add_subplot()
+    #         im = ax.imshow(cm,vmax=mx,vmin=0)
 
-            ## Forward pass
-            #   (1,100)          (1,197)          (197,1)    
-            for j in range(100): 
-                v1[0][j] = np.dot(np.append(x,1), np.transpose(self.h1_weights[j]))
-                v1[0][j] = self.actfx(v1[0][j])
+    #         for j in range(10):
+    #             for k in range(10):
+    #                 text = ax.text(k,j,cm[j,k],ha="center", va="center", color="w",fontsize=10)
 
-            #   (1,10)          (1,101)  (101,1)    
-            for j in range(10):
-                o[0][j] = np.dot(v1,     np.transpose(self.out_weights[j]))
-                o[0][j] = o[0][j]
+    #         plt.colorbar(im)
+    #         plt.ylabel('Actual')
+    #         plt.xlabel('Prediction')
+    #         plt.xticks(np.arange(0, 10, 1))
+    #         plt.yticks(np.arange(0, 10, 1))
+    #         ax.set_ylim(10-0.5, -0.5)
+    #         print("\nDisplaying confusion matrix...\n")
+    #         plt.savefig(self.cwd+'/cm_mat_'+self.desc+'.jpg')
+    #         if(show):    
+    #             plt.show()
+    #         plt.close()
             
-            self.pred_res[l] = o[0]
-            self.pred_ind[l] = i
-
-        self.did_i_test = True
-        self.classify()
-
-
-    #############################
-    # Classifier
-    def classify(self, ):
-        #print(self.pred_res[:10])
-
-        for i in range(self.test_dim):
-            mxi = np.argmax(self.pred_res[i])
-            self.pred_res[i]      = np.zeros((10,))
-            self.pred_res[i][mxi] = 1
-
-
-    #############################
-    # Plot error over updates
-    def plot_error(self, show=True):  
-        if(self.did_i_train):
-            fig = plt.figure(figsize=(11,9))
-            plt.plot(self.err_exp)
-            plt.ylabel('error')
-            plt.xlabel('updates')
-            print("\nDisplaying error plot...\n")
-            plt.savefig(self.cwd+'/error_plot_'+self.desc+'.jpg')
-            if(show):    
-                plt.show()
-            plt.close()
-        else:
-            print('\nTrain the network first!\n')
-
-
-    #############################
-    # Plot deltas over updates
-    def plot_deltas(self, show=True):  
-        if(self.did_i_train):
-            fig = plt.figure(figsize=(11,9))
-            plt.plot(self.h1_delta_full.ravel())
-            plt.ylabel('deltas')
-            plt.xlabel('updates')
-            print("\nDisplaying delta_h1 plot...\n")
-            plt.savefig(self.cwd+'/deltah1_plot_'+self.desc+'.jpg',bbox_inches='tight')
-            if(show):    
-                plt.show()
-            plt.close()
-            fig = plt.figure(figsize=(11,9))
-            plt.plot(self.ow_delta_full.ravel())
-            plt.ylabel('deltas')
-            plt.xlabel('updates')
-            print("\nDisplaying delta_ow plot...\n")
-            plt.savefig(self.cwd+'/deltaow_plot_'+self.desc+'.jpg',bbox_inches='tight')
-            if(show):
-                plt.show()
-            plt.close()
-        else:
-            print('\nTrain the network first!\n')
-
-    
-    #############################
-    # Show confusion matrix
-    def show_cm(self, show=True):
-        if(self.did_i_test):
-            cm = np.zeros((10,10))
-
-            good = 0
-            for i in range(self.test_dim):
-                ind = int(self.pred_ind[i])
-                tpi = np.argmax(self.test_labels[ind])
-                cm[tpi] += self.pred_res[i]
-
-                #score
-                if(tpi == np.argmax(self.pred_res[i])):
-                    good += 1
-
-            mx = np.max(cm)
-
-            fig = plt.figure(figsize=(9,9))
-            fig.suptitle('')
-
-            ax = fig.add_subplot()
-            im = ax.imshow(cm,vmax=mx,vmin=0)
-
-            for j in range(10):
-                for k in range(10):
-                    text = ax.text(k,j,cm[j,k],ha="center", va="center", color="w",fontsize=10)
-
-            plt.colorbar(im)
-            plt.ylabel('Actual')
-            plt.xlabel('Prediction')
-            plt.xticks(np.arange(0, 10, 1))
-            plt.yticks(np.arange(0, 10, 1))
-            ax.set_ylim(10-0.5, -0.5)
-            print("\nDisplaying confusion matrix...\n")
-            plt.savefig(self.cwd+'/cm_mat_'+self.desc+'.jpg')
-            if(show):    
-                plt.show()
-            plt.close()
-            
-            return (good/self.test_dim)
-        else:
-            print('\nTest on the network first!')
-            return 0
-
-    
-    #############################
-    # Show an image of one neuron's learned weights
-    def show_weights(self, i=0, show=True):
-        if(self.did_i_train):
-            fig = plt.figure(figsize=(9,9))
-            weights = self.h1_weights[i][:-1]
-            weights = np.reshape(weights,(14,14))
-            print("\nDisplaying learned weights...\n")
-            plt.imshow(weights)
-            plt.savefig(self.cwd+'/weights_img_'+self.desc+'.jpg')
-            if(show):
-                plt.show()
-            plt.close()
-        else:
-            print('\nTrain the network first!\n')
+    #         return (good/self.test_dim)
+    #     else:
+    #         print('\nTest on the network first!')
+    #         return 0
